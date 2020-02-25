d96f5679a2c75c5738b0275a7520cceb20a1392b
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-02-25 13:01:41 -0500

    Update MyShapes.py

--- 02_python/MyShapes.py
+++ 02_python/MyShapes.py
@@ -4,10 +4,3 @@ class RightTriangle(object):
     self.height = height
   def area(self):
     return 0.5 * self.base * self.height
-
-class Rectangle(object):
-  def __init__(self, base, height):
-    self.base = base
-    self.height = height
-  def area(self):
-    return self.base * self.height
