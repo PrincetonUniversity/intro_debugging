a776210b09f96e274eb13c4190fa6fd13061392a
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-02-25 10:07:31 -0500

    Create mybreak.py

---
+++ 02_python/mybreak.py
@@ -0,0 +1,12 @@
+"""There is no bug in this code."""
+
+def myfunc(x, y):
+  z = min(x, y)
+  return x**2 + y**2 + z**2
+
+x = sum([1 for u in '4a9d9eeJz' if u.isalpha()])
+y = 1 if x > 7 else -1
+
+breakpoint()
+
+print(myfunc(x, y))
