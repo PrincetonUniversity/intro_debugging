d17c1b24a04c3f5c0e1c87481e2326fea62d08f6
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-02-25 09:56:20 -0500

    Create no_furniture.py

---
+++ 02_python/no_furniture.py
@@ -0,0 +1,17 @@
+"""This script should print a list of non-furniture objects in
+   alphabetical order.""" 
+
+def remove_furniture(items):
+  furniture = {'couch', 'table', 'desk', 'chair'}
+  items_furniture_removed = [item for item in items if item not in furniture]
+  return items_furniture_removed
+
+# input list of items
+items = ['book', 'pencil', 'desk', 'door']
+
+# remove furniture objects from items
+items = remove_furniture(items)
+
+# print remaining items in alphabetical order
+for item in items.sort():
+  print(item)
