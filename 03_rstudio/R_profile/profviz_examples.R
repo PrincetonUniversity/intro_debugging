cda47bc4e7d885b7f65110691b298c7553fd325d
abiswas-odu <ab50@princeton.edu>
2022-01-13 08:48:02 -0500

    Added a slide on profvis and another example.

--- 03_rstudio/R_profile/profviz_examples.R
+++ 03_rstudio/R_profile/profviz_examples.R
@@ -19,3 +19,36 @@ profvis({
   }
   flights <- cbind(flights, record_delay)
 })
+
+profvis({
+  snail_sort <- function(x){
+    n<-length(x)
+    for(j in 1:(n-1)){
+      for(i in 1:(n-j)){
+        if(x[i]>x[i+1]){
+          temp<-x[i]
+          x[i]<-x[i+1]
+          x[i+1]<-temp
+        }
+      }
+    }
+    return(x)
+  }
+  sort_search <- function(search_list, query){
+    sorted_list <- snail_sort(search_list)
+    for(j in 1:(length(sorted_list)-1)){
+      if(query == sorted_list[j]){
+        return(1)
+      }
+    }
+    return(0)
+  }
+  search_list<-sample(1:10000,1000)
+  query_list<-sample(1:10000,100)
+  found_ctr <- 0
+  for(i in 1:(length(query_list)-1)){
+    if(sort_search(search_list, query_list[i]) == 1)
+      found_ctr = found_ctr + 1
+  }
+  cat('Elements found:', found_ctr, '\n')
+})
