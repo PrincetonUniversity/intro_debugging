83bbc11fd84e2b87bd4ccc58cebee5ddd3627923
abiswas-odu <ab50@princeton.edu>
2022-01-14 03:24:55 -0500

    Added comments to the portfolio example.

--- 02_python/python_debug/src/portfolio_forecast.py
+++ 02_python/python_debug/src/portfolio_forecast.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+"""
+Load the HPI file. Returns a 2 level dictionary indexed by the zipcode and the period
+"""
 def load_hpi(hpi_file):
     hpi_dict = {}
     with open(hpi_file,'r') as f_hpi:
@@ -20,18 +23,26 @@ if __name__ == '__main__':
     hpi_file = '../metadata/hpi.csv'
     portfolio_file = '../metadata/rmbs_portfolio.csv'
     hpi_dict = load_hpi(hpi_file)
-    forecast_time = 60
+    forecast_time = 120
     current_total = 0
     forecasted_total = 0
     with open(portfolio_file,'r') as f_portfolio:
         f_portfolio.readline()
+        # Read the assets and loop over them
         for line in f_portfolio:
+            # Split the string and extract the values
             str_tok = line.split(sep=',')
             current_price = np.int(str_tok[0].strip() or 0)
             zipcode = np.int(str_tok[1].strip() or 0)
             own_pct = np.float64(str_tok[2].strip() or 0)
+
+            # Calculate the current value of the asset and its contribution to the portfolio
             current_total = current_total + (current_price * own_pct / 100)
-            forecasted_price = current_price * hpi_dict[zipcode][60] / hpi_dict[zipcode][0]
+
+            # Calculate the forecasted value of the property
+            forecasted_price = current_price * hpi_dict[zipcode][forecast_time] / hpi_dict[zipcode][0]
+
+            # Calculate the weighted sum to get its contribution to the portfolio
             forecasted_total = forecasted_total + (forecasted_price * own_pct / 100)
     print('Current value of portfolio: ' + str(round(current_total,2)))
     print('Forecasted value of portfolio: ' + str(round(forecasted_total,3)))
